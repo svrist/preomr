7af2db86313f4d423a19b0fd3b04ccc0641938e0
U-SVLAPTOP\Soren Vrist <s@vrist.dk>
2009-08-10 21:45:04 +0200

    Add License source code

--- outline.py
+++ outline.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2009 Søren Bjerregaard Vrist
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 from gamera.core import RGBPixel  
 def outline(rgb_img,cc,width=2.0,color=RGBPixel(255,0,0)):
     rgb_img.draw_hollow_rect((cc.offset_x,cc.offset_y),(cc.offset_x+cc.ncols,cc.offset_y+cc.nrows),color,width)
