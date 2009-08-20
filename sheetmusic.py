c809ea58aa4db374a0855c7edc7ad7ecc2045bcc
U-SVLAPTOP\Soren Vrist <s@vrist.dk>
2009-08-20 22:32:47 +0200

    Prepare class for all my helper functions
    Instead of having all helper functions on images copied around, create a
    wrapper for "Image" and provide the needed methods.

---
+++ sheetmusic.py
@@ -0,0 +1,23 @@
+from gamera.core import * 
+from remove import remstaves,reminside,remoutside
+
+class MusicImage(object):
+    self.image  =None
+
+    def __init__(self,image):
+        self.image= image
+        self.ms = None
+
+    def without_staves(self):
+        if self.ms is None:
+            self.ms = remstaves(self.image)
+        return self.ms.image
+
+    def without_insidestaves_info(self):
+        if self.noinside is None:
+            self.noinside = reminside(self.image.image_copy())
+        return self.noinside
+
+    def __getattr__(self,name):
+        return self.image.__getattr__(name)
+
