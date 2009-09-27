f7e57b23096a2f99908759057dd9c8f2214913bf
U-svlaptop7\svrist <svrist@svlaptop7.(none)>
2009-09-27 16:05:37 +0200

    Add falsepositives in the denominator

--- dostats.py
+++ dostats.py
@@ -17,10 +17,10 @@ if __name__ == '__main__':
             print "%s has questionable filetype?(%s)"%(f,f[-4:])
 
     print "Text: %s"%text
-    print "\tFN rate: %.2f%%"%(text["falsenegative"]/float(text["found"]+text["falsenegative"])*100)
+    print "\tFN rate: %.2f%%"%(text["falsenegative"]/float(text["found"]+text["falsenegative"]-text["falsepositive"])*100)
     print "\tFP rate: %.2f%%"%(text["falsepositive"]/float(text["found"]+text["falsenegative"]-text["falsepositive"])*100)
     print 
     print "Dynamics: %s"%dynamic
-    print "\tFN rate: %.2f%%"%(dynamic["falsenegative"]/float(dynamic["found"]+dynamic["falsenegative"])*100)
+    print "\tFN rate: %.2f%%"%(dynamic["falsenegative"]/float(dynamic["found"]+dynamic["falsenegative"]-dynamic["falsepositive"])*100)
     print "\tFP rate: %.2f%%"%(dynamic["falsepositive"]/float(dynamic["found"]+dynamic["falsenegative"]-dynamic["falsepositive"])*100)
 
